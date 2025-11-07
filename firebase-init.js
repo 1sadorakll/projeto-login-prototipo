9f796213a321768adf1b8664031f4bfdc365bdd6
1sadorakll <isadorakamille04@gmail.com>
2025-11-07 17:17:35 +0000

    a

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAnWeQafpPVJ9ucrgPgc-VSbpkThOWyyIs",
+    authDomain: "projeto-login-7f43e.firebaseapp.com",
+    projectId: "projeto-login-7f43e",
+    storageBucket: "projeto-login-7f43e.firebasestorage.app",
+    messagingSenderId: "5669412589",
+    appId: "1:5669412589:web:cef05f803ed9e8e429f6e4",
+    measurementId: "G-6PVCB43TG5"
+  };
+
+firebase.initializeApp(firebaseConfig); 
+  
